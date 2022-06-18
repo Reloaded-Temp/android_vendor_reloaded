3f77e06d930f124cc74bc2da5e86969eff06fa2d
Aryaman Srivastava <aryamansrivastava895@gmail.com>
2022-06-18 22:37:56 +0530

    build: Replace obsolete ADDITIONAL_BUILD_PROPERTIES

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,9 +1,9 @@
 # Build fingerprint
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 # ReloadedOS System Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.modversion=$(RELOADED_VERSION) \
     ro.reloaded.version=$(RELOADED_VERSION_PROP) \
     ro.caf.version=$(shell grep "refs/tags/LA.QSSI" .repo/manifests/default.xml | cut -d '"' -f2 | cut -d "/" -f3)
